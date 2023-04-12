a659167a843f3b0167a6ef7f15e924b524726e09
unnati1612 <unnatijain1612@gmail.com>
2023-04-12 20:52:52 +0530

    last update

--- backend/Routes/DisplayData.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/fooddata',(req,res)=>{
-    res.send([global.food_items,global.foodCategory])
-// console.log(global.food_items)
-})
-
-module.exports = router;
